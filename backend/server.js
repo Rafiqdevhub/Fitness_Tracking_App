fd7bccf4c67582b11b9de2e7f54fad9b55547190
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-16 16:09:38 +0500

    Update server.js file to use "/api/" as the base route for UserRoutes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
-app.use("/api/user/", UserRoutes);
+app.use("/api/", UserRoutes);
 // error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
